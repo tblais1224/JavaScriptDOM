9c5248bb920502747362eeff0896beca0cdc9b83
tblais1224 <thomasblais.student@careerdevs.com>
2019-03-31 18:54:45 -0400

    version2

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const list = document.querySelector('#book-list ul');
+list.addEventListener('click', function (e) {
+    if (e.target.className == 'delete') {
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
 
-    const list = document.querySelector('#book-list ul');
-    list.addEventListener('click', function (e) {
-        if (e.target.className == 'delete') {
-            const li = e.target.parentElement;
-            list.removeChild(li);
-        }
-    });
+//add book
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function (e) {
+    //default refreshes page
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+});
