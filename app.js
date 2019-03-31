41b6afd4a59a476e9d715d4f0f3d1151c0f1c32f
tblais1224 <thomasblais.student@careerdevs.com>
2019-03-31 18:57:57 -0400

    version3

--- app.js
+++ app.js
@@ -13,4 +13,22 @@ addForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
     console.log(value);
+
+    //create elements 
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = "delete";
+    bookName.textContent = value;
+
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    //append to document  (order matters)
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 });
